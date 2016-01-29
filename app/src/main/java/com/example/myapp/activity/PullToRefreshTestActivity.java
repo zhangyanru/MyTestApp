3932f679046ce343358869446703e8f1e56d571f
张燕茹 <yanru.zhang@renren-inc.com>
2016-01-29 19:08:49 +0800

    修改了pull to refresh

--- app/src/main/java/com/example/myapp/activity/PullToRefreshTestActivity.java
+++ app/src/main/java/com/example/myapp/activity/PullToRefreshTestActivity.java
@@ -3,10 +3,13 @@ package com.example.myapp.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import com.example.myapp.R;
 import com.example.myapp.adapter.CommonAdapter;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshGridView;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 import java.util.ArrayList;
@@ -18,7 +21,7 @@ import java.util.ArrayList;
  * Email: yanru.zhang@renren-inc.com
  */
 public class PullToRefreshTestActivity extends Activity {
-    private PullToRefreshListView pullToRefreshListView;
+    private PullToRefreshGridView pullToRefreshListView;
     private ArrayAdapter arrayAdapter;
     private ArrayList<String> arrayList = new ArrayList<String>();
 
@@ -27,11 +30,32 @@ public class PullToRefreshTestActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_pull_to_refresh_test);
-        pullToRefreshListView = (PullToRefreshListView)findViewById(R.id.pull_to_refresh_list_view);
+        pullToRefreshListView = (PullToRefreshGridView)findViewById(R.id.pull_to_refresh_list_view);
         for(int i=0 ; i <10;i++){
             arrayList.add("zyr" + i);
         }
         arrayAdapter = new ArrayAdapter(this,R.layout.xlistview_item,R.id.xListView_item,arrayList);
         pullToRefreshListView.setAdapter(arrayAdapter);
+        pullToRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<GridView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<GridView> refreshView) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                pullToRefreshListView.onRefreshComplete();
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
     }
 }
